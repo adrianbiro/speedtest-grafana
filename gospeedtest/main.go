45e1e11be46c3b6fec789d35ad939998dbafa716
Adrian Biro <biroadrian@protonmail.com>
2023-02-11 15:47:31 +0100

    todo influx

--- gospeedtest/main.go
+++ gospeedtest/main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"context"
 	"fmt"
-	"github.com/influxdata/influxdb-client-go/v2"
-=======
-	"fmt"
->>>>>>> origin/go-speed
-	"github.com/showwin/speedtest-go/speedtest"
 	"os"
 	"time"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/showwin/speedtest-go/speedtest"
 )
 
 // getspeedtest get Latency Download speed ald Upload speed
-<<<<<<< HEAD
 func getspeedtest() (Lat int64, Dl float64, Ul float64) {
-=======
-func getspeedtest() (Lat time.Duration, Dl float64, Ul float64) {
->>>>>>> origin/go-speed
 	//test val
 	if len(os.Getenv("asdasddasasd")) != 0 {
 		user, _ := speedtest.FetchUserInfo()
@@ -28,7 +21,6 @@ func getspeedtest() (Lat time.Duration, Dl float64, Ul float64) {
 			s.PingTest()
 			s.DownloadTest(false)
 			s.UploadTest(false)
-<<<<<<< HEAD
 			Lat, Dl, Ul = s.Latency.Milliseconds(), s.DLSpeed, s.ULSpeed
 		}
 	} else {
@@ -37,7 +29,9 @@ func getspeedtest() (Lat time.Duration, Dl float64, Ul float64) {
 	}
 	return //8.12645ms 96.65129866971003 25.25837866052983
 }
-func writetoinfluxdb(Lat int64, Dl float64, Ul float64) {
+func writetoinfluxdb() {
+	var Lat, Dl, Ul = getspeedtest()
+
 	//todo host, password, INFLUXDB_DB = speedtest
 	client := influxdb2.NewClient("http://localhost:8086", "password")
 	writeAPI := client.WriteAPIBlocking("speedtest", "my-bucket")
@@ -48,8 +42,7 @@ func writetoinfluxdb(Lat int64, Dl float64, Ul float64) {
 	}
 }
 func main() {
-	var Lat, Dl, Ul = getspeedtest()
-	writetoinfluxdb(Lat, Dl, Ul)
+	writetoinfluxdb()
 }
 
 /*var (
@@ -66,30 +59,3 @@ func main() {
 	SPEEDTEST_INTERVAL := 3600
 	os.Getenv("HOMEPATH")
 */
-=======
-
-			Lat, Dl, Ul = s.Latency, s.DLSpeed, s.ULSpeed
-		}
-	} else {
-		Lat, Dl, Ul = (8 * time.Millisecond), 96.65129866971003, 25.25837866052983
-	}
-	return //8.12645ms 96.65129866971003 25.25837866052983
-}
-func main() {
-	/*var (
-		INFLUXDB_DB string = "INFLUXDB_HOST"
-	)*/
-	/*if os.Getenv("INFLUXDB_HOST") {
-		INFLUXDB_HOST := "influxdb"
-	}*/
-	/*
-		INFLUXDB_DB := 'speedtest'
-		INFLUXDB_USERNAME := 'root'
-		INFLUXDB_PASSWORD =: 'root'
-		SPEEDTEST_HOST := : 'local'
-		SPEEDTEST_INTERVAL := 3600
-		os.Getenv("HOMEPATH")*/
-
-	fmt.Println(getspeedtest())
-}
->>>>>>> origin/go-speed
